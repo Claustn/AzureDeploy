f9a7956e3e8438038057402e2531cb2983a7afaf
Claustn <claustn@gmail.com>
2015-12-18 10:05:54 +0100

    test Transcription

--- AzureContainerWeb/container-web-server.ps1
+++ AzureContainerWeb/container-web-server.ps1
@@ -1,10 +1,14 @@
-﻿#requires -Version 2 -Modules Hyper-V, NetNat, NetSecurity, ServerManager
+﻿#requires -Version 3 -Modules Hyper-V, NetNat, NetSecurity
 
 Param (
-	[Parameter(Mandatory = $true)]
-	[string]$containername
+    [Parameter(Mandatory = $true)]
+    [string]$containername
 )
-
+If (-Not(Test-Path -Path C:\temp)) 
+{
+    New-Item -ItemType Directory -Path C:\temp
+}
+Start-Transcript -OutputDirectory C:\temp
 # Waiting for the Custom Extension to complete before proceeding...
 
 Start-Sleep -Seconds 120
@@ -24,40 +28,40 @@ Start-Container $container -Verbose
 # Install Web-Server within the container
 Try
 {
-	$ReturnMessage = Invoke-Command -ContainerName $container.Name -RunAsAdministrator -ErrorAction Stop -ScriptBlock {
-		try
-		{
-			Install-WindowsFeature -Name Web-Server -IncludeManagementTools
-			Write-Verbose -Message 'IIS Installed'
-		}
+    $ReturnMessage = Invoke-Command -ContainerName $container.Name -RunAsAdministrator -ErrorAction Stop -ScriptBlock {
+        try
+        {
+            Install-WindowsFeature -Name Web-Server -IncludeManagementTools
+            Write-Verbose -Message 'IIS Installed'
+        }
 		
-		catch
-		{
-			Write-Verbose -Message 'Failed to Install IIS'
-			Return $_
-		}
-	} -Verbose
+        catch
+        {
+            Write-Verbose -Message 'Failed to Install IIS'
+            Return $_
+        }
+    } -Verbose
 }
 Catch
 {
-	Write-Verbose -Message 'Invoke-Command Failed'
-	Write-Verbose -Message $ReturnMessage
-	Write-Error $_
+    Write-Verbose -Message 'Invoke-Command Failed'
+    Write-Verbose -Message $ReturnMessage
+    Write-Error $_
 	
-	Write-Verbose -Message 'Trying again'
-	Invoke-Command -ContainerName $container.Name -RunAsAdministrator -ErrorAction Stop -ScriptBlock {
-		try
-		{
-			Install-WindowsFeature -Name Web-Server -IncludeManagementTools
-			Write-Verbose -Message 'IIS Installed'
-		}
+    Write-Verbose -Message 'Trying again'
+    Invoke-Command -ContainerName $container.Name -RunAsAdministrator -ErrorAction Stop -ScriptBlock {
+        try
+        {
+            Install-WindowsFeature -Name Web-Server -IncludeManagementTools
+            Write-Verbose -Message 'IIS Installed'
+        }
 		
-		catch
-		{
-			Write-Verbose -Message 'Failed to Install IIS'
-			Return $_
-		}
-	} -Verbose
+        catch
+        {
+            Write-Verbose -Message 'Failed to Install IIS'
+            Return $_
+        }
+    } -Verbose
 }
 
 
@@ -80,17 +84,18 @@ Start-Container $newcontainer -Verbose
 # Creating NAT rules and port config for container
 
 if (!(Get-NetNatStaticMapping | Where-Object -FilterScript {
-	$_.ExternalPort -eq 80
+            $_.ExternalPort -eq 80
 }))
 {
-	Add-NetNatStaticMapping -NatName 'ContainerNat' -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 172.16.0.2 -InternalPort 80 -ExternalPort 80
+    Add-NetNatStaticMapping -NatName 'ContainerNat' -Protocol TCP -ExternalIPAddress 0.0.0.0 -InternalIPAddress 172.16.0.2 -InternalPort 80 -ExternalPort 80
 }
 
 if (!(Get-NetFirewallRule | Where-Object -FilterScript {
-	$_.Name -eq 'TCP80'
+            $_.Name -eq 'TCP80'
 }))
 {
-	New-NetFirewallRule -Name 'TCP80' -DisplayName 'HTTP on TCP/80' -Protocol tcp -LocalPort 80 -Action Allow -Enabled True
+    New-NetFirewallRule -Name 'TCP80' -DisplayName 'HTTP on TCP/80' -Protocol tcp -LocalPort 80 -Action Allow -Enabled True
 }
 
 Write-Host -Object 'You are done :-)'
+Stop-Transcript
